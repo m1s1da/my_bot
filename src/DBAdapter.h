509a8be67ef314fe6aae201ae17b799ac6b77253
Daniel Sinkevich <work.sinkevich@gmail.com>
2023-07-24 16:01:09 +0300

    created DBCreator.h

--- src/DBAdapter.h
+++ src/DBAdapter.h
@@ -9,41 +9,7 @@
 #include <cstdint>
 #include <sqlite_orm/sqlite_orm.h>
 
-
-using std::string;
-using namespace sqlite_orm;
-
-struct UserMessages {
-    string user_id;
-    string guild_id;
-    uint32_t message_counter;
-    uint32_t word_counter;
-    uint32_t embed_counter;
-};
-
-struct UserVoice {
-    string user_id;
-    string guild_id;
-    uint32_t voice_timer;
-};
-
-auto make_storage_func(){
-    return make_storage("../my_bot_db",
-                        make_table("user_messages",
-                                   make_column("user_id", &UserMessages::user_id),
-                                   make_column("guild_id", &UserMessages::guild_id),
-                                   make_column("message_counter",
-                                               &UserMessages::message_counter),
-                                   make_column("word_counter", &UserMessages::word_counter),
-                                   make_column("embed_counter", &UserMessages::embed_counter),
-                                   primary_key(&UserMessages::user_id,
-                                               &UserMessages::guild_id)),
-                        make_table("user_voice",
-                                   make_column("user_id", &UserVoice::user_id),
-                                   make_column("guild_id", &UserVoice::guild_id),
-                                   make_column("message_counter", &UserVoice::voice_timer),
-                                   primary_key(&UserVoice::user_id, &UserVoice::guild_id))
-    );}
+#include "DBCreator.h"
 
 class DBAdapter {
 public:
