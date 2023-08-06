a2ada5585e2d2ebba93c073e9503bcadb3fca4c4
Daniel Sinkevich <work.sinkevich@gmail.com>
2023-08-06 12:00:18 +0300

    cmake and init.sh fix

--- init.sh
+++ init.sh
@@ -4,6 +4,7 @@ cd ./libs
 git clone https://github.com/brainboxdotcc/DPP.git
 git clone https://github.com/gabime/spdlog.git
 git clone https://github.com/adeharo9/cpp-dotenv.git
+git clone https://github.com/SRombauts/SQLiteCpp.git
 # shellcheck disable=SC2103
 cd ..
 touch ".env"
