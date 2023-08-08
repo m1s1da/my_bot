9846b70c452e8fd843b11a8fed07deef36e41d26
Daniel <sidan1994@gmail.com>
2023-08-08 13:37:24 +0300

    .

--- include/tavernbot/tavernbot.h
+++ include/tavernbot/tavernbot.h
@@ -8,6 +8,6 @@
 #include <dpp/dpp.h>
 #include <dpp/nlohmann/json.hpp>
 #include <spdlog/spdlog.h>
-#include <cpp-dotenv/dotenv.h>
+#include <dotenv.h>
 #include <SQLiteCpp/SQLiteCpp.h>
 #endif // DISCORD_BOT_DISCORDBOT_H
