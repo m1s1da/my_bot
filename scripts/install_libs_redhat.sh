437bf5909b601bde7f995a2eb01e7d06520f8a77
Daniel <sidan1994@gmail.com>
2023-08-08 15:10:19 +0300

    .

--- scripts/install_libs_redhat.sh
+++
@@ -1,8 +0,0 @@
-yum install opus #??????
-yum install wget
-wget -O dpp.rpm https://dl.dpp.dev/latest/linux-x64/rpm
-yum localinstall dpp.rpm
-rm dpp.rpm
-dnf install spdlog
-./init.sh
-./install_SQLiteCpp.sh
